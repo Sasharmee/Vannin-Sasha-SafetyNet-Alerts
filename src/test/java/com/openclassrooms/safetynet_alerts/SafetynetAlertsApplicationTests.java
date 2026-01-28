18eae565a417ac3e1b063ce7cd398c9e55d5c27b
Sasha <sasha.vannin.vdb@gmail.com>
2026-01-28 09:09:56 -0600

    Initialisation du projet SafetyNet Alerts

---
+++ src/test/java/com/openclassrooms/safetynet_alerts/SafetynetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.safetynet_alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
